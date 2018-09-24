057b6d57af4a2fd2b4e41a5459f9a2f5b6f6ef67
Lauren <llwor94@gmail.com>
2018-09-24 16:59:32 -0700

    added board id to card table

--- db/migrations/20180922133551_cards.js
+++ db/migrations/20180922133551_cards.js
@@ -3,10 +3,15 @@ exports.up = function(knex, Promise) {
     table.increments();
     table.string('title', 128).notNullable();
     table.integer('list_id').unsigned();
+    table.integer('board_id').unsigned();
     table.timestamp('created_at').defaultTo(knex.fn.now());
     table
       .foreign('list_id')
       .references('lists.id')
+      .onDelete('CASCADE');
+    table
+      .foreign('board_id')
+      .references('boards.id')
       .onDelete('CASCADE');
   });
 };
--- db/seeds/03-cards.js
+++ db/seeds/03-cards.js
@@ -5,11 +5,11 @@ exports.seed = function(knex, Promise) {
     .then(function() {
       // Inserts seed entries
       return knex('cards').insert([
-        { title: 'Test card 1', list_id: 1 },
-        { title: 'Second test card', list_id: 1 },
-        { title: 'Another test card', list_id: 2 },
-        { title: 'test card yay', list_id: 2 },
-        { title: 'gonnna test more cardz', list_id: 3 },
+        { title: 'Test card 1', list_id: 1, board_id: 1 },
+        { title: 'Second test card', list_id: 1, board_id: 1 },
+        { title: 'Another test card', list_id: 2, board_id: 1 },
+        { title: 'test card yay', list_id: 2, board_id: 1 },
+        { title: 'gonnna test more cardz', list_id: 3, board_id: 2 },
       ]);
     });
 };
